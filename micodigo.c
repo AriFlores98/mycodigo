330577f211e1bb8e5173d90e7ffc1844c357ef51
AriFlores98 <ariana_lizeth20@hotmail.com>
2023-10-03 21:03:44 -0600

    Mi primer commit

---
+++ micodigo.c
@@ -0,0 +1,12 @@
+// Simple C program to display "Hello World"
+// Header file for input output 
+#include  <stdio.h>
+//main function -
+//where the execution of program begins
+int main()
+{
+	//prints hello world
+	printf("Hello World 3");
+	return 0;
+}
+
